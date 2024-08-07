49208b6d2128aac523f2425f262a02f51f4e46b0
TallesPacheco <tallespacheco@omnus.com.br>
2024-08-07 08:31:20 -0300

    first commit

---
+++ src/Aula1/aula1.java
@@ -0,0 +1,15 @@
+package Aula1;
+
+public class aula1 {
+    public static void main(String[] args) {
+        String name = "Talles";
+        String lastName = "Pacheco";
+
+        int value1 = 10;
+        int value2 = 10;
+
+        System.out.println(name + lastName);
+        System.out.println(value1 + value2);
+
+    }
+}
